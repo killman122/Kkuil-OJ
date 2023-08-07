60dfcb0872b4039f7a08405bfb2cddee68a68cd0
Kkuil <3024067144@qq.com>
2023-08-08 00:30:29 +0800

    fix: bug and solve: spring-cloud-gateway, user-service cors question

---
+++ kkuil-oj-service/kkuil-oj-user-service/src/main/java/com/kkuil/kkuilojuserservice/mapper/TbQuestionMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.TbQuestion;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 小K
+* @description 针对表【tb_question】的数据库操作Mapper
+* @createDate 2023-08-07 22:09:39
+* @Entity generator.domain.TbQuestion
+*/
+public interface TbQuestionMapper extends BaseMapper<TbQuestion> {
+
+}
+
+
+
+
---
+++ kkuil-oj-service/kkuil-oj-user-service/src/main/java/com/kkuil/kkuilojuserservice/service/TbQuestionService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.TbQuestion;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 小K
+* @description 针对表【tb_question】的数据库操作Service
+* @createDate 2023-08-07 22:09:39
+*/
+public interface TbQuestionService extends IService<TbQuestion> {
+
+}
